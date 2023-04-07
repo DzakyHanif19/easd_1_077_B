4699060a68058a68daeb6148092b46a469d1ece5
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-04-07 10:19:57 +0700

    Add project files.

---
+++ Exercise2/Exercise2.cpp
@@ -0,0 +1,6 @@
+//1.Algoritma dibutuhkan untuk menyelesaikan suatu masalah karena serangkaian langkah atau prosedur terstruktur yg dibuat untuk memecahkan suatu masalah.
+//2.Struktur data Linear dan Struktur data Non Linear
+//3.Ukuran input, Bahasa Pemrograman, Struktur data,Lingkungan eksekusi
+//4.Dari ke 6 Algoritma yang umum digunakan,saya akan memilih Merge sort dikarenakan relatif lebih mudah dipahami dan diimplementasikan.
+//5.-Algoritma Quadratic :Inserion sort,Selection sort,Bubble sort
+// -Algoritma Loglinear :Merge sort,Quick sort
