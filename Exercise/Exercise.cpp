627148f253736923c2e83c1cd84b33487f66ed7d
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-04-07 10:29:57 +0700

    arr diganti dengan nama panggilan

--- Exercise/Exercise.cpp
+++ Exercise/Exercise.cpp
@@ -3,3 +3,7 @@ using namespace std;
 
 const int MAX_SIZE = 97; // 77 + 20 - 2 x 15 + 10 + 20 (Nim:77)
 
+void merge(int jaky[], int low, int mid, int high) {    //arr diganti dengan nama panggilan
+    int i = low, DA = mid + 1, k = low;     //Nama saya : Dzaky Hanif Annafi
+    int b[MAX_SIZE];
+
